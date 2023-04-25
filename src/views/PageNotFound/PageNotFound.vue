15037d39c01b52cfa16224f2398a04476f05ccf7
liyoudi <1469854373@qq.com>
2023-04-26 01:40:18 +0800

    feat: 404页面优化

--- src/views/PageNotFound/PageNotFound.vue
+++ src/views/PageNotFound/PageNotFound.vue
@@ -1,5 +1,60 @@
 <template>
-  <div>404</div>
+  <div class="_404">
+    <h2 class="m-0">
+      抱歉，页面未找到，
+      <span>{{ tim }}</span>
+      s后自动跳转到
+      <a href="javascript:;" @click="goHome">首页</a>
+    </h2>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<script setup>
+import { onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+const tim = ref(5)
+var times = null
+onMounted(() => {
+  times = setInterval(() => {
+    tim.value--
+    if (tim.value === 0) {
+      goHome()
+    }
+  }, 1000)
+})
+
+const goHome = () => {
+  clearInterval(times)
+  router.replace({
+    path: '/',
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+._404 {
+  width: 100vw;
+  height: 100vh;
+  background-color: #f7f7f7;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  .m-0 {
+    width: 90%;
+    font-size: 0.45rem;
+    font-weight: 600;
+    span {
+      color: #dcd178;
+      font-weight: 800;
+      font-size: 0.5rem;
+    }
+    a {
+      color: #000506;
+      text-decoration: underline;
+    }
+  }
+}
+</style>
